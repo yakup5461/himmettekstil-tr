bb9eda74bee8ef517bb42febeee51456d54b3a36
yakup5461 <bilisimyakupberber@gmail.com>
2024-02-29 15:20:59 +0300

    edit lesson 18

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/assets/styles.css';
+
 import { createApp } from "vue";
 import App from "@/App.vue" ;
 import router from "@/Router/index.js";
