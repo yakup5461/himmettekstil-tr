b115bf4269839824e36fdc136c75c9dfd0b42cb1
yakup5461 <bilisimyakupberber@gmail.com>
2024-02-29 16:25:34 +0300

    lesson 18

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express();
+const port = 3000 ;
+
+
+
+app.get("/",(req , res)=>(
+    res.send('Hellooo 5!')
+));
+
+app.listen(port, () => {
+    console.log(`Server is listening port: ${port}`);
+  });
